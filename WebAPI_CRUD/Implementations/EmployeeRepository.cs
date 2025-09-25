59d10dc37cd1bd4cc782515ff2f2a5302198d934
adityakhilare04 <aditya.khilare04@gmail.com>
2025-09-26 00:17:07 +0530

    EmployeeRepository

---
+++ WebAPI_CRUD/Implementations/EmployeeRepository.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPI_CRUD.Interfaces;
+using WebAPI_CRUD.Models;
+
+namespace WebAPI_CRUD.Implementations
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private readonly AppDbContext _context;
+        public EmployeeRepository(AppDbContext context)
+        {
+            this._context = context;
+        }
+        public async Task<Employee> AddEmployee(Employee employee)
+        {
+            var result = await _context.AddAsync(employee);
+            await _context.SaveChangesAsync();
+            return result.Entity;
+        }
+
+        public async Task<bool> DeleteEmployee(Guid id)
+        {
+            var employee = await _context.Employees.FirstOrDefaultAsync(x => x.Id == id);
+            if (employee == null)
+            { 
+                return false;
+            }
+            _context.Employees.Remove(employee);
+            await _context.SaveChangesAsync();
+            return Task.CompletedTask.IsCompletedSuccessfully;
+        }
+
+        public async Task<Employee?> GetEmployee(Guid id)
+        {
+            return await _context.Employees.FirstOrDefaultAsync(x => x.Id == id) ?? null;
+        }
+
+        public async Task<Employee?> GetEmployeeByEmail(string email)
+        {
+            return await _context.Employees.FirstOrDefaultAsync(x => x.Email == email) ?? null;
+        }
+
+        public async Task<IEnumerable<Employee>> GetEmployees()
+        {
+            return await _context.Employees.ToListAsync();
+        }
+
+        public async Task<IEnumerable<Employee>> Search(string name, Gender? gender)
+        {
+            var query = _context.Employees.AsEnumerable();
+            if (!string.IsNullOrEmpty(name))
+            {
+                query = query.Where(x => name.Contains(x.FirstName) || name.Contains(x.LastName));
+            }
+            if(gender != null)
+            {
+                query = query.Where(x => x.Gender == gender);
+            }
+            await Task.CompletedTask;
+            return query;
+        }
+
+        public async Task<Employee?> UpdateEmployee(Employee employee)
+        {
+            var existingEmployee = await _context.Employees.FirstOrDefaultAsync(x => x.Id == employee.Id);
+            if (existingEmployee != null)
+            {
+                existingEmployee.Email = employee.Email;
+                existingEmployee.FirstName = employee.FirstName;
+                existingEmployee.LastName = employee.LastName;
+                existingEmployee.Gender = employee.Gender;
+                existingEmployee.DateOfBirth = employee.DateOfBirth;
+                existingEmployee.DepartmentId = employee.DepartmentId;
+
+                var result = _context.Employees.Update(existingEmployee);
+                await _context.SaveChangesAsync();
+                return result.Entity;
+            }
+            return null;
+        }
+    }
+}
---
+++ WebAPI_CRUD/Interfaces/IEmployeeRepository.cs
@@ -0,0 +1,15 @@
+﻿using WebAPI_CRUD.Models;
+
+namespace WebAPI_CRUD.Interfaces
+{
+    public interface IEmployeeRepository
+    {
+        Task<IEnumerable<Employee>> Search(string name, Gender? gender);
+        Task<IEnumerable<Employee>> GetEmployees();
+        Task<Employee?> GetEmployee(Guid id);
+        Task<Employee?> GetEmployeeByEmail(string email);
+        Task<Employee> AddEmployee(Employee employee);
+        Task<Employee> UpdateEmployee(Employee employee);
+        Task<bool> DeleteEmployee(Guid id);
+    }
+}
