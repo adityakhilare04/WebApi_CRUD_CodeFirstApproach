611b757f6ff337ec0fe6478ed527cf59e7a4c615
adityakhilare04 <aditya.khilare04@gmail.com>
2025-09-25 23:27:39 +0530

    Setting up the connection string and DbContext class

--- WebAPI_CRUD/Program.cs
+++ WebAPI_CRUD/Program.cs
@@ -1,7 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using WebAPI_CRUD.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<AppDbContext>(options => 
+        options.UseSqlServer(builder.Configuration.GetConnectionString("DBConnection")));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
