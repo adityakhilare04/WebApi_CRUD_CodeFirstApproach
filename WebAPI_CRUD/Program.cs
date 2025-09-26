032e6cde2ca212b34521612c5fca8cdf2770939e
adityakhilare04 <aditya.khilare04@gmail.com>
2025-09-26 15:39:57 +0530

    Register the Dependencies into the Program.cs

--- WebAPI_CRUD/Program.cs
+++ WebAPI_CRUD/Program.cs
@@ -1,4 +1,6 @@
 using Microsoft.EntityFrameworkCore;
+using WebAPI_CRUD.Implementations;
+using WebAPI_CRUD.Interfaces;
 using WebAPI_CRUD.Models;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,6 +8,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddDbContext<AppDbContext>(options => 
         options.UseSqlServer(builder.Configuration.GetConnectionString("DBConnection")));
+
+builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+builder.Services.AddScoped<IDepartmentRepository, DepartmentRepository>();
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
