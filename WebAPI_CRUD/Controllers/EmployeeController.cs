efae38014fabc282a5dff09edb656346c250c9d9
adityakhilare04 <aditya.khilare04@gmail.com>
2025-09-26 20:20:31 +0530

    Creating RESTApi from Employees

---
+++ WebAPI_CRUD/Controllers/EmployeeController.cs
@@ -0,0 +1,141 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Mime;
+using WebAPI_CRUD.Interfaces;
+using WebAPI_CRUD.Models;
+
+namespace WebAPI_CRUD.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+        public EmployeeController(IEmployeeRepository employeeRepository)
+        {
+                this._employeeRepository = employeeRepository;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [Produces(MediaTypeNames.Application.Json)]
+        public async Task<IActionResult> GetAllEmployees()
+        {
+            return Ok(await _employeeRepository.GetEmployees());
+        }
+
+        [HttpGet("{id:guid}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [Produces(MediaTypeNames.Application.Json)]
+        public async Task<IActionResult> GetEmployeeById([FromQuery] Guid id)
+        {
+            try
+            {
+                var employee = await _employeeRepository.GetEmployee(id);
+                if(employee == null)
+                {
+                    return NotFound();
+                }
+                return Ok(employee);
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetEmployeeByEmail([FromQuery] string email)
+        {
+            try
+            {
+                var employee = await _employeeRepository.GetEmployeeByEmail(email);
+                if (employee == null)
+                {
+                    return NotFound();
+                }
+                return Ok(employee);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        [HttpGet("search")]
+        public async Task<IActionResult> Search([FromQuery] string name, [FromQuery] Gender? gender)
+        {
+            try
+            {
+                return Ok(await _employeeRepository.Search(name, gender));
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [Produces(MediaTypeNames.Application.Json)]
+        public async Task<IActionResult> AddEmployee([FromBody] Employee employee)
+        {
+            try
+            {
+                if (employee == null)
+                {
+                    return BadRequest();
+                }
+                var result = await _employeeRepository.AddEmployee(employee);
+                return CreatedAtAction(nameof(GetEmployeeById), new { id = result.Id }, result);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteEmployee([FromQuery] Guid id)
+        {
+            try
+            {
+                var result = await _employeeRepository.DeleteEmployee(id);
+                if (!result)
+                {
+                    return NotFound();
+                }
+                return NoContent();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        [HttpPut]
+        public async Task<IActionResult> UpdateEmployee([FromBody] Employee employee)
+        {
+            try
+            {
+                if (employee == null)
+                {
+                    return BadRequest();
+                }
+                var existingEmployee = await _employeeRepository.GetEmployee(employee.Id);
+                if (existingEmployee == null)
+                {
+                    return NotFound();
+                }
+                var result = await _employeeRepository.UpdateEmployee(employee);
+                return Ok(result);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+}
