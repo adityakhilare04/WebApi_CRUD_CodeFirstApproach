81bd2fa65475bdb381b8bb19d778651919f2f8d5
adityakhilare04 <aditya.khilare04@gmail.com>
2025-09-28 23:16:04 +0530

    Added one class library project and one xUnit Project

---
+++ NetworkUtility.Tests/NetworkUtilityTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkUtility.Tests
+{
+    
+    public class NetworkUtilityTests
+    {
+        [Fact]
+        public void NetworkUtility_GetNetworkInfo_ReturnsExpectedString()
+        {
+            // Arrange
+            NetworkUtility networkUtility = new NetworkUtility();
+            string expected = "Network Info";
+
+            // Act
+            var result = NetworkUtility.GetNetworkInfo();
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
