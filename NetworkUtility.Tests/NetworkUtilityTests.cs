2332b70b1d1d99d2d077f5c00ce40debf45ad370
adityakhilare04 <aditya.khilare04@gmail.com>
2025-10-09 23:19:55 +0530

    Setting up the Web API Test Project

--- NetworkUtility.Tests/NetworkUtilityTests.cs
+++ NetworkUtility.Tests/NetworkUtilityTests.cs
@@ -1,105 +1,98 @@
 ﻿using FakeItEasy;
 using NetworkUtility.Interface;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace NetworkUtility.Tests
+namespace NetworkUtility.Tests;
+
+public class NetworkUtilityTests
 {
-    
-    public class NetworkUtilityTests
+    private readonly NetworkUtility _networkUtility;
+    private readonly IDnsRepository _dnsRepository;
+    public NetworkUtilityTests()
     {
-        private readonly NetworkUtility _networkUtility;
-        private readonly IDnsRepository _dnsRepository;
-        public NetworkUtilityTests()
-        {
-            _dnsRepository = A.Fake<IDnsRepository>();
-            _networkUtility = new NetworkUtility(_dnsRepository);
-        }
+        _dnsRepository = A.Fake<IDnsRepository>();
+        _networkUtility = new NetworkUtility(_dnsRepository);
+    }
 
-        [Fact]
-        //Function Naming convention --> classname_methodname_expectedbehavior
-        public void NetworkUtility_GetNetworkInfo_ReturnsExpectedString() 
-        {
-            // Arrange
-            string expected = "Network Info";
+    [Fact]
+    //Function Naming convention --> classname_methodname_expectedbehavior
+    public void NetworkUtility_GetNetworkInfo_ReturnsExpectedString() 
+    {
+        // Arrange
+        string expected = "Network Info";
 
-            // Act
-            var result = _networkUtility.GetNetworkInfo();
+        // Act
+        var result = _networkUtility.GetNetworkInfo();
 
-            // Assert
-            Assert.Equal(expected, result);
-        }
+        // Assert
+        Assert.Equal(expected, result);
+    }
 
-        [Fact]
-        public void NetworkUtility_Add_ReturnsCorrectSum()
-        {
-            //Arrange
-            int a = 5;
-            int b = 10;
-            int expectedSum = 15;
-            // Act
-            var result = _networkUtility.Add(a, b);
-            // Assert
-            Assert.Equal(expectedSum, result);
-        }
+    [Fact]
+    public void NetworkUtility_Add_ReturnsCorrectSum()
+    {
+        //Arrange
+        int a = 5;
+        int b = 10;
+        int expectedSum = 15;
+        // Act
+        var result = _networkUtility.Add(a, b);
+        // Assert
+        Assert.Equal(expectedSum, result);
+    }
 
-        [Theory]
-        [InlineData(5, 10, 15)]
-        [InlineData(-5, -10, -15)]
-        [InlineData(10, -5, 5)]
-        public void NetworkUtility_Add_ExpectsCorrectSum(int a, int b, int expectedResult)
-        {
-            //Arrange
+    [Theory]
+    [InlineData(5, 10, 15)]
+    [InlineData(-5, -10, -15)]
+    [InlineData(10, -5, 5)]
+    public void NetworkUtility_Add_ExpectsCorrectSum(int a, int b, int expectedResult)
+    {
+        //Arrange
 
-            // Act
-            var result = _networkUtility.Add(a, b);
+        // Act
+        var result = _networkUtility.Add(a, b);
 
-            // Assert
-            Assert.Equal(expectedResult, result);
-        }
+        // Assert
+        Assert.Equal(expectedResult, result);
+    }
 
-        [Fact]
-        public void NetworkUtility_GetDateTime_ReturnsCurrentDateTime()
-        {
-            // Arrange
-            DateTime beforeCall = DateTime.Now;
-            // Act
-            var result = _networkUtility.GetDateTime();
-            DateTime afterCall = DateTime.Now;
-            // Assert
-            Assert.True(result >= beforeCall && result <= afterCall);
-            Assert.InRange(result, beforeCall, afterCall);
-            Assert.IsType<DateTime>(result);
-        }
+    [Fact]
+    public void NetworkUtility_GetDateTime_ReturnsCurrentDateTime()
+    {
+        // Arrange
+        DateTime beforeCall = DateTime.Now;
+        // Act
+        var result = _networkUtility.GetDateTime();
+        DateTime afterCall = DateTime.Now;
+        // Assert
+        Assert.True(result >= beforeCall && result <= afterCall);
+        Assert.InRange(result, beforeCall, afterCall);
+        Assert.IsType<DateTime>(result);
+    }
 
-        [Fact]
-        public void NetworkUtility_GetEmployee_ReturnsExpectedEmployee()
-        {
-            // Arrange
-            int expectedId = 1;
-            string expectedName = "Virat Kohli";
-            // Act
-            var result = _networkUtility.GetEmployee();
-            // Assert
-            Assert.NotNull(result);
-            Assert.IsType<Employee>(result);
-            Assert.Equal(expectedId, result.Id);
-            Assert.Equal(expectedName, result.Name);
-        }
+    [Fact]
+    public void NetworkUtility_GetEmployee_ReturnsExpectedEmployee()
+    {
+        // Arrange
+        int expectedId = 1;
+        string expectedName = "Virat Kohli";
+        // Act
+        var result = _networkUtility.GetEmployee();
+        // Assert
+        Assert.NotNull(result);
+        Assert.IsType<Employee>(result);
+        Assert.Equal(expectedId, result.Id);
+        Assert.Equal(expectedName, result.Name);
+    }
 
-        [Fact]
-        public void NetworkUtility_GetStatus_ReturnsSuccessWhenDnsRepositoryReturnsTrue()
-        {
-            // Arrange
-            A.CallTo(() => _dnsRepository.GetStatus()).Returns(true);
-            string expected = "Success";
-            // Act
-            var result = _networkUtility.GetStatus();
-            // Assert
-            Assert.Equal(expected, result);
-        }
+    [Fact]
+    public void NetworkUtility_GetStatus_ReturnsSuccessWhenDnsRepositoryReturnsTrue()
+    {
+        // Arrange
+        A.CallTo(() => _dnsRepository.GetStatus()).Returns(true);
+        string expected = "Success";
+        // Act
+        var result = _networkUtility.GetStatus();
+        // Assert
+        Assert.Equal(expected, result);
     }
 }
---
+++ WebAPI_CRUD.Tests/Controllers/EmployeeControllerTests.cs
@@ -0,0 +1,23 @@
+﻿using FakeItEasy;
+using WebAPI_CRUD.Controllers;
+using WebAPI_CRUD.Interfaces;
+
+namespace WebAPI_CRUD.Tests.Controllers;
+
+public class EmployeeControllerTests
+{
+    private readonly IEmployeeRepository _employeeRepository;
+    private readonly EmployeeController _employeeController;
+    public EmployeeControllerTests()
+    {
+        _employeeRepository = A.Fake<IEmployeeRepository>();
+        _employeeController = new EmployeeController(_employeeRepository);
+    }
+
+    [Fact]
+    public void EmployeeController_GetAllEmployees_ReturnsOkResult()
+    {
+
+    }
+
+}
