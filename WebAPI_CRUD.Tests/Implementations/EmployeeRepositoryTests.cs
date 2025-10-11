3bc8e661375063c42a3e5538c7e4b1412013dbac
adityakhilare04 <aditya.khilare04@gmail.com>
2025-10-11 14:55:32 +0530

    Unit test for DeleteEmployee

--- WebAPI_CRUD.Tests/Implementations/EmployeeRepositoryTests.cs
+++ WebAPI_CRUD.Tests/Implementations/EmployeeRepositoryTests.cs
@@ -14,13 +14,8 @@ public class EmployeeRepositoryTests
         return new AppDbContext(options);
     }
 
-    [Fact]
-    public async Task EmployeeRepository_AddEmployee_ShouldAddEmployee()
-    {
-        // Arrange
-        var context = GetInMemoryContext();
-        var repository = new EmployeeRepository(context);
-        var newEmployee = new EmployeeDto
+    private static EmployeeDto GetSampleEmployee()
+        => new()
         {
             FirstName = "John",
             LastName = "Doe",
@@ -29,6 +24,14 @@ public class EmployeeRepositoryTests
             DepartmentId = Guid.NewGuid(),
             Gender = Gender.Male
         };
+
+    [Fact]
+    public async Task EmployeeRepository_AddEmployee_ShouldAddEmployee()
+    {
+        // Arrange
+        var context = GetInMemoryContext();
+        var repository = new EmployeeRepository(context);
+        var newEmployee = GetSampleEmployee();
 
         // Act
         var result = await repository.AddEmployee(newEmployee);
@@ -39,5 +42,22 @@ public class EmployeeRepositoryTests
         Assert.Equal("John", result.FirstName);
         Assert.NotEqual(Guid.Empty, result.Id);
 
+    }
+
+    [Fact]
+    public async Task EmployeeRepository_DeleteEmployee_ShouldDeleteEmployee()
+    {
+        // Arrange
+        var context = GetInMemoryContext();
+        var repository = new EmployeeRepository(context);
+        var newEmployee = GetSampleEmployee();
+        var entity = await repository.AddEmployee(newEmployee);
+
+        // Act
+        var result = await repository.DeleteEmployee(entity.Id);
+
+        // Assert
+        Assert.IsType<bool>(result);
+        Assert.True(result);
     }
 }
